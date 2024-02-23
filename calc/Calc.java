6ad9bba574fcb2afa7b3b1c22b2fe45ab8d0d3ee
Arthur Asasira <asasiraarthur@gmail.com>
2024-02-23 19:12:59 +0300

    updated and added aloop

--- calc/Calc.java
+++ calc/Calc.java
@@ -2,32 +2,33 @@ package calc;
 import java.util.*;
 public class Calc {
     public static Scanner scanner=new Scanner(System.in);
-    double x1,x2,x0;
+   
     double f1(double a)
     {
 
         double fx =(a*a*a)-(5*a)+1;
         return fx;
     }
-   
-    
-    double x2()
+    double x2(double x1,double x0)
     {
+        double xx=0;
+        for (int i=0;i<6;i++){
+            xx=x0-(f1(x0)*(x1-x0))/(f1(x1)-f1(x0));
+            x0=x1;
+            x1=xx;
 
-        double x22=x0-(f1(x0)*(x1-x0))/(f1(x1)-f1(x0));
-        
-        return x22;
-    }
+        }
+        return xx;
+     }
 public static void main(String args[])
 {
+    double x1,x0;
     Calc calc=new Calc();
     System.out.println("Enter x0:");
-     calc.x0=scanner.nextDouble();
+     x0=scanner.nextDouble();
     System.out.println("Enter x1:");
-    calc.x1=scanner.nextDouble();
-    System.out.println("x2="+calc.x2());
-    System.out.println();
-    System.out.println(calc.f1(calc.x2()));
+    x1=scanner.nextDouble();
+    System.out.println("x2="+calc.x2(x1,x0));
 
 }
     
